01b6cf0245dbf25bc9ccce4ec1f4fc5883d13417
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-10-03 00:11:15 +0530

    Add App Logo

--- app/src/main/java/com/amarnath/sicassembler/file.kt
+++ app/src/main/java/com/amarnath/sicassembler/file.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
